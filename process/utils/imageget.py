d6f50877ad01ba85ee51255358edeab5f775d834
Fguinyard3 <74794439+Fguinyard3@users.noreply.github.com>
2022-02-21 15:39:47 -0500

    Update imageget.py

--- process/utils/imageget.py
+++ process/utils/imageget.py
@@ -1,27 +1,27 @@
-from email.mime import image
 import shutil
 from urllib import response
 import shutil
 import requests
-from PIL import Image as img
 import os
 from pathlib import Path
 
 
 class ImageGetter():
     def __init__(self):
+        #Tells each instance what to set the recieving folder to
         self.directory = Path(__file__).resolve().parent
         self.storage = os.path.join(self.directory, 'content') 
     def get_catimg(self):
-
+        #Here, n will be determine based on the the amount of files in the directory.
+        #This helps the function create duplicate named images just by formatting the end of the file name 
         n = sum(1 for d in os.listdir(self.storage) if os.path.isfile(os.path.join(self.storage, d)))
 
 
         file_name = 'cat_image{}.jpg'.format(n+1) 
-
-        response = requests.get('https://thecatapi.com/api/images/get?format=src&type=gif?api_key=4b44be7a-6781-4962-8bfd-dd7141edd60f', stream=True)
+        #Get image from thecatapi's API
+        response = requests.get('https://thecatapi.com/api/images/get?format=src&type=gif?api_key=<YOUR API KEY FOR thecatapi >', stream=True)
         print(response.text)
-
+        #Write file and moves it to content folder
         if response.status_code == 200:
             with open(file_name,'wb') as i:
                 i.write(response.content)
@@ -31,15 +31,13 @@ class ImageGetter():
 
         shutil.move(file_name, self.storage)
     def retrieve_imgs(self):
+        #Simple loop that calls get_catimg() until the content folder has 10 files. Still has bugs to be worked out.
         while True:
             ImageGetter.get_catimg(self)
             if len(os.listdir(self.storage)) >= 10:
                 break
 
-        
-
-bot = ImageGetter()
-bot.retrieve_imgs()
+       
 
 
 
